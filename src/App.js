d29d9650c43f3a38d0c23a2224776c99e79ae669
Jordan <jordanelley@hotmail.com>
2022-12-09 12:27:19 +1300

    added animations back in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import myNameClipArt from './images/myName.png'
 import codesClipArt from './images/codes.png'
 import './App.css';
+import 'animate.css'
 
 function App() {
   return (
     <div className="App">
-        <img src={myNameClipArt} />
-        <img src={codesClipArt} />
+        <h1>test</h1>
+        <img src={myNameClipArt} className="animate__animated animate__slideInLeft animate__slower"/>
+        <img src={codesClipArt} className="animate__animated animate__slideInRight animate__slower"/>
     </div>
   );
 }
