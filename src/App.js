e921bcd7d29697318e318411c9683b2c812660b8
Jordan <jordanelley@hotmail.com>
2022-12-09 12:41:32 +1300

    removed test code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'animate.css'
 function App() {
   return (
     <div className="App">
-        <h1>test</h1>
         <img src={myNameClipArt} className="animate__animated animate__slideInLeft animate__slower"/>
         <img src={codesClipArt} className="animate__animated animate__slideInRight animate__slower"/>
     </div>
